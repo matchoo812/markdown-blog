56206cbbb45311b0a0351246966bd926a4f0ba26
Matt Ballard <mattjballard81@gmail.com>
2021-11-21 01:37:46 +0900

    add Post component and configure to parse markdown frontmatter

---
+++ components/Post.js
@@ -0,0 +1,40 @@
+import Link from 'next/link';
+import Image from 'next/image';
+
+export default function Post({ post }) {
+  // console.log(post);
+  const { author, author_image, category, cover_image, date, excerpt, title } =
+    post.frontmatter;
+  return (
+    <div className='w-full px-10 py-6 bg-white rounded-lg shadow-md mt-6'>
+      <Image src={cover_image} height={420} width={600} alt='image' className='mb-4' />
+      <div className='flex justify-between items-center'>
+        <span className='font-light text-gray-600'>{date}</span>
+        <div>{category}</div>
+      </div>
+
+      <div className='mt-2'>
+        <Link href={`/blog/${post.slug}`}>
+          <a className='text-2l text-gray-700 font-bold hover:underline'>{title}</a>
+        </Link>
+        <p className='mt-2 text-gray-600'>{excerpt}</p>
+      </div>
+
+      <div className='flex justify-between items-center mt-6'>
+        <Link href={`/blog/${post.slug}`}>
+          <a className='text-gray-900 hover:text-blue-600'>Read More</a>
+        </Link>
+        <div className='flex items-center'>
+          <Image
+            src={author_image}
+            alt={author}
+            width={40}
+            height={40}
+            className='w-10 h-10 object-cover rounded-full hidden sm:block'
+          />
+          <h3 className='ml-2 text-gray-700 font-bold'>{author}</h3>
+        </div>
+      </div>
+    </div>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,43 @@
+import fs from 'fs';
+import path from 'path';
+import matter from 'gray-matter';
+import Link from 'next/link';
 import Layout from '../components/Layout';
+import Post from '../components/Post';
 
-export default function Home() {
+export default function HomePage({ posts }) {
+  // console.log(posts);
   return (
     <Layout>
-      <h1>Hey there, Bloggy Bloggerton!</h1>
+      <h1 className='text-5xl border-b-4 p-5'>Latest Posts</h1>
+
+      <div className='grid md:grid-cols-2 lg:grid-cols-3 gap-5'>
+        {posts.map((post, index) => (
+          <Post post={post} key={index} />
+        ))}
+      </div>
+      <Link href='/blog'>
+        <a className='block text-center border border-gray-500 text-gray-800 rounded-md py-4 my-5 transition duration-500 ease select-none hover:text-white hover:bg-gray-900 focus:outline-none focus:shadow-outline w-full'>
+          View All Posts
+        </a>
+      </Link>
     </Layout>
   );
+}
+
+export async function getStaticProps() {
+  const files = fs.readdirSync(path.join('posts'));
+  const posts = files.map((file) => {
+    const slug = file.replace('.md', '');
+    const mdWithMeta = fs.readFileSync(path.join('posts', file), 'utf-8');
+
+    // use a colon to rename data being destructured out of function
+    const { data: frontmatter } = matter(mdWithMeta);
+
+    return { slug, frontmatter };
+  });
+
+  return {
+    props: { posts },
+  };
 }
